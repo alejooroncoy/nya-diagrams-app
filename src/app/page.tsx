b812b036f032734d834a64619922c48743d02e93
dannielsss <gamekik92@gmail.com>
2024-03-10 19:22:24 -0600

    feat: :sparkles: add go to dashboard button in the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,11 @@ import Link from 'next/link';
 import styles from './page.module.css';
 
 import LoginPanel from '@/components/LoginPanel';
+import { GetUser } from '@/actions/UserActions';
 
 export default async function Home() {
+  const user = await GetUser();
+
   return (
     <main className={styles.main}>
       <div className={styles.main__card}>
@@ -19,10 +22,17 @@ export default async function Home() {
         <Link href="">Create diagram</Link>
       </div>
       <div className={styles.main__arrow}></div>
-      <div className={styles.main__card}>
-        <h1>For more features log in</h1>
-        <LoginPanel />
-      </div>
+      {user ? (
+        <div className={`${styles.main__card} ${styles.main__user}`}>
+          <h1>{user.user_metadata.full_name}</h1>
+          <Link href="/dashboard">Go to dashboard</Link>
+        </div>
+      ) : (
+        <div className={styles.main__card}>
+          <h1>For more features log in</h1>
+          <LoginPanel />
+        </div>
+      )}
     </main>
   );
 }
